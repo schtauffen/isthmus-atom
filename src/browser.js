387915cb22063735a8ea61ca07a5a5d5b28eae33
Zach Dahl <z.schtauffen@gmail.com>
2017-09-22 20:38:00 -0500

    fix browser build

--- src/browser.js
+++ src/browser.js
@@ -1,2 +1,3 @@
-import * as modules from './'
-window.isthmus = Object.assign(window.isthmus || {}, modules)
+import * as atom from './'
+window.isthmus = window.isthmus || {}
+window.isthmus.atom = atom
